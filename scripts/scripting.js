66d61e7fb7d7aab5f4c0206d1c0ed13d633da039
Howard <Howard@gmail.com>
2023-03-27 15:47:39 -0700

    add text

--- scripts/scripting.js
+++ scripts/scripting.js
@@ -6,3 +6,21 @@ function alertUser() {
 }
 
 buttonRef.addEventListener("click", alertUser, { once: true });
+
+function changeBGPink() {
+	document.body.style.background = "pink";
+}
+
+buttonRef.addEventListener("click", changeBGPink);
+
+function changeText() {
+	// if the button says click me, change text to clicked
+	// else if it says clicked, change it to click
+	if (buttonRef.textContent === "Click Me!") {
+		buttonRef.textContent = "Clicked";
+	} else {
+		buttonRef.textContent = "Click Me!";
+	}
+}
+
+buttonRef.addEventListener("click", changeText);
